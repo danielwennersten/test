5000fa22ab127b0ae3a05aa692d718627fca4cf5
daniel.wennersten <daniel.wennersten@gcisd.net>
2020-01-23 12:10:40 -0600

    Creating Ship Class

--- core/src/com/test/Testgame.java
+++ core/src/com/test/Testgame.java
@@ -11,58 +11,34 @@ import com.badlogic.gdx.math.Rectangle;
 
 public class Testgame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Ship myShip;
 	Texture asteroid;
-	Rectangle ShipHitBox;
-	Rectangle AsteroidHitBox;
-	Intersector HitBoxIntersection;
+	Rectangle asteroidHitBox;
 	float x,y;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("ship.png");
+		myShip = new Ship("ship.png","asteroid.png",x,y,64f,64f);
 		asteroid = new Texture("asteroid.png");
-		ShipHitBox = new Rectangle(x,y,64,64);
-		AsteroidHitBox = new Rectangle(300,220, 32, 32);
-		System.out.println(x);
-		HitBoxIntersection = new Intersector();
+		asteroidHitBox = new Rectangle(200,300,32,32);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			x = x + 5;
-			System.out.println(x);
-		}
-
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			x = x - 5;
-		}
-
-		if (Gdx.input.isKeyPressed(Input.Keys.UP))  {
-			y = y + 5;
-		}
-
-		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-			y = y - 5;
-		}
-		boolean isIntersecting = Intersector.overlaps(ShipHitBox,AsteroidHitBox);
-        if (isIntersecting){
-            System.out.println("We've been hit, Captain!!!");
-        }
+		myShip.moveShip();
+		myShip.checkCollision(myShip.getShipHitBox(),asteroidHitBox);
 		batch.begin();
-		batch.draw(img, x, y);
-		batch.draw(asteroid, 300, 220);
+		batch.draw(myShip.getShipImage(),myShip.getX(), myShip.getY());
+		batch.draw(asteroid,200,300);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		myShip.getShipImage().dispose();
 	}
 }
